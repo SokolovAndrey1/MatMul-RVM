a889af607c9e49ce4d85124f9f6309eba3fd697f
AveZorgen <78862872+AveZorgen@users.noreply.github.com>
2024-05-15 01:00:55 +0300

    fix registers configs

--- lib/src/gemm_blocked_rvm.c
+++ lib/src/gemm_blocked_rvm.c
@@ -4,7 +4,7 @@
 
 #define BLOCK_SIZE 4
 
-static inline void process_block_4x4(const size_t k, const float *A, const size_t lda, const float *B, const size_t ldb, float *C, const size_t ldc);
+static inline void process_block_4x4(const size_t k, const float *A, const size_t lda, const float *B, const size_t ldb, float *C, const size_t ldc, float *BT);
 
 /**
  * Multiplies two matrices A and B with dimensions n x m and m x k respectively
@@ -20,30 +20,28 @@ static inline void process_block_4x4(const size_t k, const float *A, const size_
 extern void gemm_block4x4_rvm(const float *A, const float *B, float *C, const size_t n, const size_t m, const size_t k) {
     mcfgm(BLOCK_SIZE);
     mcfgn(BLOCK_SIZE);
-    mcfgk(m * sizeof(*A));
+    mcfgk(BLOCK_SIZE * sizeof(*A));
+    float *BT = malloc(BLOCK_SIZE * m * sizeof(*B));
     for (size_t j = 0; j < k; j += BLOCK_SIZE) {     /* Loop over the columns of C */
         for (size_t i = 0; i < n; i += BLOCK_SIZE) { /* Loop over the rows of C */
-            process_block_4x4(m, &A[i * m], m, &B[j], k, &C[(i * k) + j], k);
+            process_block_4x4(m, &A[i * m], m, &B[j], k, &C[(i * k) + j], k, BT);
         }
     }
 }
 
-static inline void process_block_4x4(const size_t m, const float *A, const size_t lda, const float *B, const size_t ldb, float *C, const size_t ldc) {
-    volatile float BT[BLOCK_SIZE * k];
-    for (size_t i = 0; i < k; i++) {
+static inline void process_block_4x4(const size_t m, const float *A, const size_t lda, const float *B, const size_t ldb, float *C, const size_t ldc, float *BT) {
+    for (size_t i = 0; i < m; i++) {
         for (size_t j = 0; j < BLOCK_SIZE; j++) {
-            BT[j * k + i] = B[i * ldb + j];
+            BT[j * m + i] = B[i * ldb + j];
         }
     }
-
-    mfloat32_t ma = mld_f32(A, lda * sizeof(*A));
-    mfloat32_t mb = mld_f32(BT, k * sizeof(*BT));
+    mfloat32_t ma;
+    mfloat32_t mb;
     mfloat32_t ans = mld_f32(C, ldc * sizeof(*C));
-
-    ans = fmmacc_mf32(ans, ma, mb);  // ans = ans + ma * mb.T ???
-    volatile float Ct[BLOCK_SIZE * BLOCK_SIZE];
-    mst_f32_mf32(Ct, BLOCK_SIZE * sizeof(*Ct), ans);  // mst_f32_mf32(C, ldc * sizeof(*C), ans) zeroes the elements between stride ???
-    for (size_t i = 0; i < BLOCK_SIZE; i++) {
-        memcpy(&C[i * ldc], &Ct[i * BLOCK_SIZE], BLOCK_SIZE * sizeof(*Ct));
+    for (size_t p = 0; p < m; p += BLOCK_SIZE) {
+        ma = mld_f32(A + p, lda * sizeof(*A));
+        mb = mld_f32(BT + p, lda * sizeof(*B));
+        ans = fmmacc_mf32(ans, ma, mb);
+        mst_f32_mf32(C, ldc * sizeof(*C), ans);
     }
 }
