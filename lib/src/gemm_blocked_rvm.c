a0d17db53f20dfebc186b346ecbcae0cf3c9a95d
Александр Возженников <sash0k.v@yandex.ru>
2024-05-15 17:31:36 +0300

    попытка решения

--- lib/src/gemm_blocked_rvm.c
+++ lib/src/gemm_blocked_rvm.c
@@ -1,6 +1,30 @@
 #include "gemm.h"
+#include <riscv_matrix.h>
 
 #define BLOCK_SIZE 4
+
+static inline void process_block_4x4(const size_t k, const float *A, const size_t lda, const float *B, const size_t ldb, float *C, const size_t ldc) {
+    long stride = k * sizeof(float32_t);
+    printf("k = %d stride = %d\n", k, stride);
+
+    // загрузка блоков    
+    mfloat32_t ma = mld_f32(A, stride);
+    mfloat32_t mb = mld_f32(B, stride);
+    mfloat32_t mc = mld_f32(C, stride);
+
+    //print_matrix_reg("|ma|", ma, k, k);
+    //print_matrix_reg("|mb|", mb, k, k);
+    //print_matrix_reg("|mc|", mc, k, k);
+
+    
+    // вычисление результата
+    mfloat32_t res = fmmacc_mf32(mc, ma, mb);
+
+    print_matrix_reg("|iteration res|", res, k, k);
+
+    // выгрузка
+    mst_f32_mf32(C, stride, res);
+}
 
 /**
  * Multiplies two matrices A and B with dimensions n x m and m x k respectively
@@ -15,6 +39,22 @@
  */
 extern void gemm_block4x4_rvm(const float *A, const float *B, float *C, const size_t n, const size_t m, const size_t k)
 {
-    // TODO: Implement Gemm using THEAD RISC-V matrix extension.
-    gemm_ref(A, B, C, n, m, k);    
+    //print_matrix(A, n, m);
+    //print_matrix(B, m, k);
+
+    
+    // конфигурация матричных регистров
+    mcfgm(BLOCK_SIZE);
+    mcfgn(BLOCK_SIZE);
+    mcfgk(BLOCK_SIZE * sizeof(float32_t));
+    
+    for (size_t j = 0; j < k; j += BLOCK_SIZE) { /* Loop over the columns of C */
+        for (size_t i = 0; i < n; i += BLOCK_SIZE) { /* Loop over the rows of C */
+            //printf("i = %d, j = %d, m = %d, k = %d\n", i,j,m,k);
+            process_block_4x4(m, &A[i*m], m, &B[j], k, &C[(i*k) + j], k);
+        }
+    }
+    
+    //printf("rvm:\n");
+    //print_matrix(C, n, k);
 }
