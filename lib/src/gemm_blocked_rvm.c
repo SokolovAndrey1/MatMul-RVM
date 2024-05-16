8995d80c2244f21bad71844e92ddab28a23e85db
Александр Возженников <sash0k.v@yandex.ru>
2024-05-16 14:14:25 +0300

    доработки: добавлено транспонирование второй матрицы, скорректированы размеры матриц

--- lib/src/gemm_blocked_rvm.c
+++ lib/src/gemm_blocked_rvm.c
@@ -4,26 +4,29 @@
 #define BLOCK_SIZE 4
 
 static inline void process_block_4x4(const size_t m, const float *A, const size_t lda, const float *B, const size_t ldb, float *C, const size_t ldc) {
-    long stride = k * sizeof(float32_t);
-    printf("k = %d stride = %d\n", k, stride);
-
     // загрузка блоков    
-    mfloat32_t ma = mld_f32(A, stride);
-    mfloat32_t mb = mld_f32(B, stride);
-    mfloat32_t mc = mld_f32(C, stride);
+    mfloat32_t ma = mld_f32(A, lda * sizeof(*A));
+    mfloat32_t mb = mld_f32(B, ldb * sizeof(*B));
+    mfloat32_t mc = mld_f32(C, ldc * sizeof(float));
 
-    //print_matrix_reg("|ma|", ma, k, k);
-    //print_matrix_reg("|mb|", mb, k, k);
-    //print_matrix_reg("|mc|", mc, k, k);
-
+    //print_matrix_reg("|ma|", ma, m, m);
+    //print_matrix_reg("|mb|", mb, m, m);
+    //print_matrix_reg("|mc|", mc, m, m);
     
     // вычисление результата
     mfloat32_t res = fmmacc_mf32(mc, ma, mb);
 
-    print_matrix_reg("|iteration res|", res, k, k);
+    //print_matrix_reg("|iteration res|", res, m, m);
 
     // выгрузка
-    mst_f32_mf32(C, stride, res);
+    mst_f32_mf32(C, ldc * sizeof(*C), res);
+}
+
+static inline void transpose_4x4(const float *B, float *T, const size_t m, const size_t k) {
+    for(size_t i = 0; i < m; ++i)
+        for(size_t j = 0; j < k; ++j)
+            T[j*m+i] = B[i*k+j];
+    
 }
 
 /**
@@ -39,22 +42,24 @@ static inline void process_block_4x4(const size_t m, const float *A, const size_
  */
 extern void gemm_block4x4_rvm(const float *A, const float *B, float *C, const size_t n, const size_t m, const size_t k)
 {
-    //print_matrix(A, n, m);
+    // транспонирование матрицы B
+    float T[sizeof(B)] = {0};
+    transpose_4x4(B, T, m, k);
+    
     //print_matrix(B, m, k);
-
+    //print_matrix(T, k, m);
     
     // конфигурация матричных регистров
     mcfgm(BLOCK_SIZE);
     mcfgn(BLOCK_SIZE);
-    mcfgk(BLOCK_SIZE * sizeof(float32_t));
+    mcfgk(BLOCK_SIZE * sizeof(float));
     
     for (size_t j = 0; j < k; j += BLOCK_SIZE) { /* Loop over the columns of C */
         for (size_t i = 0; i < n; i += BLOCK_SIZE) { /* Loop over the rows of C */
-            //printf("i = %d, j = %d, m = %d, k = %d\n", i,j,m,k);
-            process_block_4x4(m, &A[i*m], m, &B[j], k, &C[(i*k) + j], k);
+            process_block_4x4(m, &A[i*m], m, &T[j], k, &C[(i*k) + j], k);
         }
     }
     
-    //printf("rvm:\n");
-    //print_matrix(C, n, k);
+    ////printf("rvm:\n");
+    ////print_matrix(C, n, k);
 }
